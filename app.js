25f6dbab9786438fc560d55f517883b42ddfc903
Jeffery Park <jp@Jefferys-MBP.cogeco.local>
2022-02-09 13:36:52 -0500

    Add canvas context to draw line

---
+++ app.js
@@ -0,0 +1,54 @@
+const canvas = document.getElementById('jsCanvas');
+// get canvas' 2D rendering context
+// context to manipulate pixels inside canvas
+const ctx = canvas.getContext('2d');
+
+// to give size to the pixel manipulation
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = '#2c2c2c';
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function stopPainting(e) {
+	painting = false;
+}
+
+function startPainting() {
+  painting = true;
+}
+
+function onMouseMove(e) {
+	// offset from canvas
+	const x = e.offsetX;
+	const y = e.offsetY;
+
+  if(!painting) {
+    // creates path as mouse moves
+    ctx.beginPath();
+    // moves path to x, y position of the mouse
+    // starting point of line
+    ctx.moveTo(x, y);
+  } else {
+    // creates line from the previous position of the path to the position of the line
+    // end point of line
+    // happens every time mouse is moved
+    ctx.lineTo(x, y);
+    // creates line after creating path
+    ctx.stroke()
+  }
+}
+
+function onMouseDown(e) {
+	painting = true;
+	console.log(e);
+}
+
+if (canvas) {
+	canvas.addEventListener('mousemove', onMouseMove);
+	canvas.addEventListener('mousedown', startPainting);
+	canvas.addEventListener('mouseup', stopPainting);
+	canvas.addEventListener('mouseleave', stopPainting);
+}
